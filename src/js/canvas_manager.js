637d4fe35543604834099c7415e5351ae3c9731d
fabio-pastore <fabio.pastoree@gmail.com>
2026-03-22 19:29:18 +0100

    created Canvas class

--- src/js/canvas_manager.js
+++ src/js/canvas_manager.js
@@ -1,104 +1,114 @@
 const DEFAULT_BRUSH_COLOR = "#000000"
 const DEFAULT_BRUSH_SIZE = 3;
+class Canvas { 
 
-var canvas;
-var color_picker;
-var brush_size_picker;
-var clear_button;
-var context;
-var is_pointer_down;
-var last_x_pos, last_y_pos;
-var stroke_color;
-var stroke_width;
+    // private fields
+    #canvas;
+    #color_picker;
+    #brush_size_picker;
+    #clear_button;
+    #context;
+    #is_pointer_down;
+    #last_x_pos;
+    #last_y_pos;
 
-function getPointerPos(e) {
-    let client_rect = canvas.getBoundingClientRect();
-    return {
-        x: (e.clientX - client_rect.left) * (canvas.width / client_rect.width),
-        y: (e.clientY - client_rect.top) * (canvas.height / client_rect.height)
-    };
-}
+    // public fields
+    stroke_color;
+    stroke_width;
 
-function pointerDownHandler(e) {
-
-    if (e.button == 1) { // NOTE: e.button == 1 if and only if the middle mouse button was clicked (we will use this to reset the canvas for the time being)
-        e.preventDefault();
-        clearCanvas();
+    constructor() {
+        this.#canvas = document.getElementById("boardCanvas");
+        this.#color_picker = document.getElementById("brushColor");
+        this.#brush_size_picker = document.getElementById("brushSize");
+        this.#clear_button = document.getElementById("clearButton");
+        this.#context = this.#canvas.getContext("2d");
+        this.#is_pointer_down = false;
+        this.#last_x_pos = null;
+        this.#last_y_pos = null;
+        this.stroke_color = DEFAULT_BRUSH_COLOR;
+        this.stroke_width = DEFAULT_BRUSH_SIZE;
+        this.#initCanvas();
     }
 
-    else {
-        is_pointer_down = true;
-        let pointer_pos = getPointerPos(e);
-        last_x_pos = pointer_pos.x;
-        last_y_pos = pointer_pos.y;
+    #initCanvas() {
+
+        this.#canvas.width = this.#canvas.offsetWidth;
+        this.#canvas.height = this.#canvas.offsetHeight;
+        this.#canvas.style.touchAction = "none";
+
+        this.#context.strokeStyle = this.stroke_color;
+        this.#context.lineWidth = this.stroke_width;
+        this.#context.lineCap = "round";
+        this.#context.lineJoin = "round";
+
+        this.#color_picker.addEventListener("input", () => this.setBrushColor());
+        this.#brush_size_picker.addEventListener("input", () => this.setBrushSize());
+        this.#clear_button.addEventListener("click", () => this.clearCanvas());
+
+        this.#canvas.addEventListener("pointerdown", (e) => this.#pointerDownHandler(e));
+        this.#canvas.addEventListener("pointermove", (e) => this.#pointerMoveHandler(e));
+        this.#canvas.addEventListener("pointerup", (e) => this.#pointerUpHandler(e));
+        this.#canvas.addEventListener("pointerleave", (e) => this.#pointerUpHandler(e));
     }
-}
 
-function pointerMoveHandler(e) {
-
-    let pointer_pos = getPointerPos(e);
-    let new_x_pos = pointer_pos.x;
-    let new_y_pos = pointer_pos.y;
-
-    if (is_pointer_down) {
-        context.beginPath();
-        context.moveTo(last_x_pos, last_y_pos);
-        context.lineTo(new_x_pos, new_y_pos);
-        context.stroke();
-        last_x_pos = new_x_pos;
-        last_y_pos = new_y_pos;
+    #getPointerPos(e) {
+        let client_rect = this.#canvas.getBoundingClientRect();
+        return {
+            x: (e.clientX - client_rect.left) * (this.#canvas.width / client_rect.width),
+            y: (e.clientY - client_rect.top) * (this.#canvas.height / client_rect.height)
+        };
     }
+
+    #pointerDownHandler(e) {
+
+        if (e.button == 1) { // NOTE: e.button == 1 if and only if the middle mouse button was clicked (we will use this to reset the canvas for the time being)
+            e.preventDefault();
+            this.clearCanvas();
+        }
+
+        else {
+            this.#is_pointer_down = true;
+            let pointer_pos = this.#getPointerPos(e);
+            this.#last_x_pos = pointer_pos.x;
+            this.#last_y_pos = pointer_pos.y;
+        }
+    }
+
+    #pointerMoveHandler(e) {
+
+        let pointer_pos = this.#getPointerPos(e);
+        let new_x_pos = pointer_pos.x;
+        let new_y_pos = pointer_pos.y;
+
+        if (this.#is_pointer_down) {
+            this.#context.beginPath();
+            this.#context.moveTo(this.#last_x_pos, this.#last_y_pos);
+            this.#context.lineTo(new_x_pos, new_y_pos);
+            this.#context.stroke();
+            this.#last_x_pos = new_x_pos;
+            this.#last_y_pos = new_y_pos;
+        }
+    }
+
+    #pointerUpHandler(e) {
+        this.#is_pointer_down = false;
+    }
+
+    clearCanvas() {
+        this.#context.clearRect(0, 0, this.#context.canvas.width, this.#context.canvas.height);
+        this.#context.beginPath(); // discard any previous undrawn lines
+    }
+
+    setBrushColor() {
+        this.#context.strokeStyle = this.#color_picker.value;
+    }
+
+    setBrushSize() {
+        this.#context.lineWidth = this.#brush_size_picker.value;
+    }
+
 }
 
-function pointerUpHandler(e) {
-    is_pointer_down = false;
-}
+const MY_CANVAS = new Canvas();
 
-function clearCanvas() {
-    context.clearRect(0, 0, context.canvas.width, context.canvas.height);
-    context.beginPath(); // discard any previous undrawn lines
-}
-
-function setBrushColor() {
-    context.strokeStyle = color_picker.value;
-}
-
-function setBrushSize() {
-    context.lineWidth = brush_size_picker.value;
-}
-
-function init_canvas_manager() {
-    canvas = document.getElementById("boardCanvas");
-    canvas.width = canvas.offsetWidth;
-    canvas.height = canvas.offsetHeight;
-    canvas.style.touchAction = "none";
-
-    is_pointer_down = false;
-    stroke_color = DEFAULT_BRUSH_COLOR;
-    stroke_width = DEFAULT_BRUSH_SIZE;
-
-    context = canvas.getContext("2d");
-    context.strokeStyle = stroke_color;
-    context.lineWidth = stroke_width;
-    context.lineCap = "round";
-    context.lineJoin = "round";
-
-
-    color_picker = document.getElementById("brushColor");
-    color_picker.addEventListener("input", setBrushColor);
-
-    brush_size_picker = document.getElementById("brushSize");
-    brush_size_picker.addEventListener("input", setBrushSize);
-
-    clear_button = document.getElementById("clearButton");
-    clear_button.addEventListener("click", clearCanvas);
-
-    canvas.addEventListener("pointerdown", pointerDownHandler);
-    canvas.addEventListener("pointermove", pointerMoveHandler);
-    canvas.addEventListener("pointerup", pointerUpHandler);
-    canvas.addEventListener("pointerleave", pointerUpHandler);
-    return 0;
-}
-
-init_canvas_manager();
 
